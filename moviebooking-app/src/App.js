00a9b5e85545d5daacaed33dc193072ef84d627a
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-04-05 21:58:03 -0500

    Update App.js
    added routes for the pages

--- moviebooking-app/src/App.js
+++ moviebooking-app/src/App.js
@@ -2,15 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
-
-
+import Navbar from './components/Navbar';
+import { BrowserRouter , Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
 function App() {
+
   return (
-    <div className="App">
-      <Register/>
-      <Login />
-    
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar/>
+        <Routes>
+          <Route path = "/" element={<Home/>} exact/>
+          <Route path = "/Register" element={<Register/>}/>
+          <Route path = "/Login" element={<Login/>}/>
+        </Routes>
+        
+      </div>
+    </BrowserRouter>
   );
 }
 
