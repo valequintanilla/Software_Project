0015f951799b880b06f86ce1b996d658a975bc0f
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-04-05 20:11:21 -0500

    added Register , login

---
+++ moviebooking-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Register/>
+      <Login />
+    
+    </div>
+  );
+}
+
+export default App;
