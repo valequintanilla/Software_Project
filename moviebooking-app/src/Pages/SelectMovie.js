fe1ac8630f511df3eee9679d132f2f3d1db253d2
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-04-19 12:21:52 -0500

    Create SelectMovie.js

---
+++ moviebooking-app/src/Pages/SelectMovie.js
@@ -0,0 +1,8 @@
+import {useRef, useState, useEffect} from "react";
+//Viewing the selected movie , this is where the tickets will be purchased
+const SelectMovie = () =>{
+    return(
+        <h1>Selected Movie Screen</h1>
+    )
+}
+export default SelectMovie;
