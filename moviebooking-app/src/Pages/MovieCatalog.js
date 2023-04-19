0350e515e14d20d39b725a0247184bdff52a39e7
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-04-19 12:21:48 -0500

    Create MovieCatalog.js

---
+++ moviebooking-app/src/Pages/MovieCatalog.js
@@ -0,0 +1,8 @@
+import {useRef, useState, useEffect} from "react";
+//Catalog to browse the movies available 
+const MovieCatalog = () =>{
+    return(
+        <h1>Movie Catalog</h1>
+    )
+}
+export default MovieCatalog;
