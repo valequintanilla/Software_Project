da9f5cc8b66b1979c92b2e0acfa1948022a5da26
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-04-30 01:02:27 -0500

    Delete MovieCatalog.js

--- moviebooking-app/src/Pages/MovieCatalog.js
+++
@@ -1,8 +0,0 @@
-import {useRef, useState, useEffect} from "react";
-//Catalog to browse the movies available 
-const MovieCatalog = () =>{
-    return(
-        <h1>Movie Catalog</h1>
-    )
-}
-export default MovieCatalog;
