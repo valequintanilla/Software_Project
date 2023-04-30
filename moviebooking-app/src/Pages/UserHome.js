d3ca122500362210581f87ad1564f251fd25019c
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-04-30 01:03:35 -0500

    Update UserHome.js
    linked the browse movies button to the movie catalog page

--- moviebooking-app/src/Pages/UserHome.js
+++ moviebooking-app/src/Pages/UserHome.js
@@ -1,16 +1,18 @@
-import {useRef, useState, useEffect} from "react";
+import {Link} from 'react-router-dom';
 import BrowseImg from './images/Browse.png';
 import BrowseImg2 from './images/Browse-2.png';
 import BrowseImg3 from './images/Browse-3.png';
 //user home page 
+
 const UserHome = () =>{
     return(
         <html>
             <p>
                 <h1>User Home Page</h1>
-                <button><img src = {BrowseImg}/></button>
-                <button><img src = {BrowseImg2}/></button>
-                <button><img src = {BrowseImg3}/></button>
+                <Link to = '/browsemovies'><button id = "user-home" ><img src = {BrowseImg} id = "user-home"/></button></Link>
+                <Link to = '/upcomingmovies' ><button id = "user-home"><img src = {BrowseImg2} id = "user-home"/></button></Link>
+                <Link to = '/usertickets'><button id = "user-home"><img src = {BrowseImg3} id = "user-home"/></button></Link>
+                
             </p>
 
         </html>
