36e73fb4eb50432ae1305a037159dea8f67ad3b8
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-04-30 19:38:06 -0500

    moved the file to pages

--- moviebooking-app/src/Pages/MovieCatalog.js
+++ moviebooking-app/src/Pages/MovieCatalog.js
@@ -1,38 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MovieList from './MovieList';
-import MovieListHeading from './MovieListHeading';
-import SearchBox from './SearchBox';
-import AddShoppingCart from '../BrowseMovie/AddShoppingCart';
-import RemoveShoppingCart from '../BrowseMovie/RemoveShoppingCart';
-import SelectedMovie from './SelectMovie';
+import MovieList from './BrowseMovie/MovieList';
+import MovieListHeading from './BrowseMovie/MovieListHeading';
+import SearchBox from './BrowseMovie/SearchBox';
+import AddShoppingCart from './BrowseMovie/AddShoppingCart';
+import RemoveShoppingCart from './BrowseMovie/RemoveShoppingCart';
+import SelectedMovie from './BrowseMovie/SelectMovie';
 import {Link} from 'react-router-dom';
+import {getMovies} from '../API_Calls/API.js'
 const MovieCatalog = () => {
-	const [movies, setMovies] = useState([{
-		Title: "Star Wars: Episode IX - The Rise of Skywalker",
-		Year: "2019",
-		imdbID: "tt2527338",
-		Type: "movie",
-		Poster: "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg"
-	},{
-		Title: "Avengers: Endgame",
-		Year: "2019",
-		imdbID: "tt4154796",
-		Type: "movie",
-		Poster: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
-	},{
-		Title: "Cruella",
-		Year: "2021",
-		imdbID: "tt3228774",
-		Type: "movie",
-		Poster: "https://m.media-amazon.com/images/M/MV5BOWI5YTUxOWEtZmRiZS00ZmQxLWE2NzctYTRiODA2NzE1ZjczXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
-	},{
-		Title: "The Super Mario Bros. Movie",
-		Year: "2023",
-		imdbID: "tt6718170",
-		Type: "movie",
-		Poster: "https://m.media-amazon.com/images/M/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_SX300.jpg"
-	}
+	const [movies, setMovies] = useState([//{
+	// 	Title: "Star Wars: Episode IX - The Rise of Skywalker",
+	// 	Year: "2019",
+	// 	imdbID: "tt2527338",
+	// 	Type: "movie",
+	// 	Poster: "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg"
+	// },{
+	// 	Title: "Avengers: Endgame",
+	// 	Year: "2019",
+	// 	imdbID: "tt4154796",
+	// 	Type: "movie",
+	// 	Poster: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+	// },{
+	// 	Title: "Cruella",
+	// 	Year: "2021",
+	// 	imdbID: "tt3228774",
+	// 	Type: "movie",
+	// 	Poster: "https://m.media-amazon.com/images/M/MV5BOWI5YTUxOWEtZmRiZS00ZmQxLWE2NzctYTRiODA2NzE1ZjczXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
+	// },{
+	// 	Title: "The Super Mario Bros. Movie",
+	// 	Year: "2023",
+	// 	imdbID: "tt6718170",
+	// 	Type: "movie",
+	// 	Poster: "https://m.media-amazon.com/images/M/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_SX300.jpg"
+	// }
 	]);
 	const [shoppingCart, setCart] = useState([]);
 	const [searchValue, setSearchValue] = useState('');
@@ -55,7 +56,10 @@ const MovieCatalog = () => {
 	// useEffect(() => {
 	// 	getCurrentCatalog(searchValue);
 	// }, [searchValue]);
-
+	useEffect(() => {
+		movies = getMovies();
+		setMovies([movies]);
+	},[]);
     //store in local storage
 	useEffect(() => {
 		const movieCart = JSON.parse(
