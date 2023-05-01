f87999bc5bce59153c7dd3767db91f75d4af6f41
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-04-30 20:46:48 -0500

    called api

--- moviebooking-app/src/Pages/BrowseMovie/MovieList.js
+++ moviebooking-app/src/Pages/BrowseMovie/MovieList.js
@@ -7,7 +7,7 @@ const MovieList = (props) => {
 		<>
 			{props.movies.map((movie, index) => (
 				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
+					<img src={movie.poster_url} alt='movie'></img>
 					<div
 						onClick={() => props.handleShoppingCartClick(movie)}
 						className='overlay d-flex align-items-center justify-content-center'
