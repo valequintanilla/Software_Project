f2b3bbea495aff31b25b24bfd2fe980f4858c08a
julesgar <87442019+julesgar@users.noreply.github.com>
2023-05-02 13:17:33 -0500

    Update ManageShow.js

--- moviebooking-app/src/Pages/ManageShow.js
+++ moviebooking-app/src/Pages/ManageShow.js
@@ -1,30 +1,81 @@
-
 import React, { useState, useEffect } from 'react';
-import { getMovies } from '../API_Calls/API';
+import { Link } from 'react-router-dom';
+import { getMovies, deleteMovie } from '../API_Calls/API';
 import { Movie } from '../classes';
 
 const ManageShows = () => {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [deleteMode, setDeleteMode] = useState(false);
 
   useEffect(() => {
     getMovies().then((fetchedMovies) => {
+      console.log('Fetched movies: ', fetchedMovies);
       setMovies(fetchedMovies);
+    })
+    .catch((error) => {
+      console.error('Error fetching movies:', error);
     });
   }, []);
+
+  const handleDelete = async () => {
+    if (!selectedMovie) return;
+    const status = await deleteMovie(selectedMovie);
+    console.log('Movie deleted with status:', status);
+    setMovies(movies.filter(movie => movie.id !== selectedMovie));
+    setSelectedMovie(null);
+    setDeleteMode(false);
+  };
 
   return (
     <div>
       <h1>Manage Show</h1>
       {movies.map((movie) => (
-        <div key={movie.id}>
-          <h2>{movie.title}</h2>
-          <p>Release Date: {movie.release_date}</p>
-          <img src={movie.poster_url} alt={movie.title} />
-        </div>
+        <Link to={`/edit-show/${movie.id}`} key={movie.id} style={{ textDecoration: 'none', color: 'inherit' }}>
+          <div
+            style={selectedMovie === movie.id && deleteMode ? { backgroundColor: 'red' } : {}}
+            onClick={() => deleteMode && setSelectedMovie(movie.id)}
+          >
+            <h2>{movie.title}</h2>
+            <p>Release Date: {movie.release_date}</p>
+            <img src={movie.poster_url} alt={movie.title} />
+          </div>
+        </Link>
       ))}
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+        <button
+          style={{ backgroundColor: 'white', color: 'black', padding: '10px' }}
+          onClick={() => setDeleteMode(!deleteMode)}
+        >
+          Remove
+        </button>
+        {deleteMode && (
+          <div>
+            <button
+              style={{ backgroundColor: 'white', color: 'black', padding: '10px' }}
+              onClick={() => setDeleteMode(false)}
+            >
+              Cancel
+            </button>
+            <button
+              style={{ backgroundColor: 'white', color: 'black', padding: '10px' }}
+              onClick={handleDelete}
+            >
+              Delete
+            </button>
+          </div>
+        )}
+        <div>
+          <Link to="/add-show">
+            <button style={{ backgroundColor: 'white', color: 'black', padding: '10px', marginRight: '10px' }}>Add</button>
+          </Link>
+          <Link to="/adminhome">
+            <button style={{ backgroundColor: 'white', color: 'black', padding: '10px' }}>Homepage</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default ManageShows;
-
