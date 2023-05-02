3f4a78dd42d155fb656d93caca133868bbf0c60b
julesgar <87442019+julesgar@users.noreply.github.com>
2023-05-02 14:28:39 -0500

    Update Payment.js

--- moviebooking-app/src/Pages/Payment.js
+++ moviebooking-app/src/Pages/Payment.js
@@ -1,123 +1,119 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Ticket } from '../classes';
+import { bookTicket } from '../API_Calls/API';
 
-const Payment = () => {
-  const [paymentMethod, setPaymentMethod] = useState(null);
+const Payment = ({ shoppingCart }) => {
+  const [paymentMethod, setPaymentMethod] = useState('');
+  const [selectedTheater, setSelectedTheater] = useState('');
+  const [time, setTime] = useState('');
+  const [seat, setSeat] = useState('');
+  const [owner, setOwner] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Implement payment processing based on the selected payment method
-    console.log('Payment submitted');
+  const navigate = useNavigate();
+
+  const handleConfirmPurchase = async () => {
+    if (!selectedTheater || !seat || !time || !owner) {
+      alert("Please fill in all required fields.");
+      return;
+    }
+
+    for (const movie of shoppingCart) {
+      const ticket = new Ticket(movie.id, selectedTheater, time, seat, owner);
+
+      try {
+        const status = await bookTicket(ticket);
+        console.log(`Ticket booking status: ${status}`);
+      } catch (err) {
+        console.error('Error booking ticket:', err);
+      }
+    }
+
+    // Redirect the user to the UserHome page after successful purchase.
+    navigate('/UserHome');
   };
-
-  const formStyle = {
-    display: 'grid',
-    gap: '10px',
-    gridTemplateColumns: '100px 300px',
-    justifyContent: 'center',
-    alignItems: 'center',
-  };
-
-  const inputStyle = {
-    width: '100%',
-  };
-
-  const renderCreditDebitForm = () => (
-    <div style={formStyle}>
-      <label htmlFor="cardNumber">Card Number:</label>
-      <input id="cardNumber" type="text" style={inputStyle} />
-      <label htmlFor="expirationDate">Expiration Date:</label>
-      <input id="expirationDate" type="text" style={inputStyle} />
-      <label htmlFor="cvc">CVC:</label>
-      <input id="cvc" type="text" style={inputStyle} />
-    </div>
-  );
-
-  const renderPayPalForm = () => (
-    <div style={formStyle}>
-      <label htmlFor="paypalEmail">PayPal Email:</label>
-      <input id="paypalEmail" type="text" style={inputStyle} />
-      <label htmlFor="paypalPassword">PayPal Password:</label>
-      <input id="paypalPassword" type="password" style={inputStyle} />
-    </div>
-  );
 
   return (
-    <div
-      style={{
-        backgroundColor: 'red',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <div>
       <h1>Payment</h1>
-      <form onSubmit={handleSubmit}>
+      <div>
+        <button onClick={() => setPaymentMethod('credit')}>Credit</button>
+        <button onClick={() => setPaymentMethod('debit')}>Debit</button>
+        <button onClick={() => setPaymentMethod('paypal')}>PayPal</button>
+      </div>
+      {paymentMethod === 'credit' && (
         <div>
-          <button
-            style={{
-              backgroundColor: 'white',
-              color: 'black',
-              marginRight: '10px',
-            }}
-            onClick={() => setPaymentMethod('credit')}
-          >
-            Credit
-          </button>
-          <button
-            style={{
-              backgroundColor: 'white',
-              color: 'black',
-              marginRight: '10px',
-            }}
-            onClick={() => setPaymentMethod('debit')}
-          >
-            Debit
-          </button>
-          <button
-            style={{
-              backgroundColor: 'white',
-              color: 'black',
-            }}
-            onClick={() => setPaymentMethod('paypal')}
-          >
-            PayPal
-          </button>
+          <h2>Credit Card Information</h2>
+          <input type="text" placeholder="Card Number" />
+          <input type="text" placeholder="Expiration Date" />
+          <input type="text" placeholder="CVV" />
         </div>
-
-        {paymentMethod === 'credit' || paymentMethod === 'debit' ? (
-      renderCreditDebitForm()
-    ) : paymentMethod === 'paypal' ? (
-      renderPayPalForm()
-    ) : null}
-
+      )}
+      {paymentMethod === 'debit' && (
         <div>
-          <button
-            style={{
-              backgroundColor: 'white',
-              color: 'black',
-              marginRight: '10px',
-              padding: '10px',
-            }}
-            onClick={() => console.log('Cancel button clicked')}
-          >
-            Cancel
-          </button>
-          <button
-            style={{
-              backgroundColor: 'white',
-              color: 'black',
-              padding: '10px',
-            }}
-            type="submit"
-          >
-            Confirm Purchase
-          </button>
+          <h2>Debit Card Information</h2>
+          <input type="text" placeholder="Card Number" />
+          <input type="text" placeholder="Expiration Date" />
+          <input type="text" placeholder="CVV" />
         </div>
-      </form>
+      )}
+      {paymentMethod === 'paypal' && (
+        <div>
+          <h2>PayPal Information</h2>
+          <input type="text" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+      )}
+      <div>
+        <label htmlFor="theater">Theater Location:</label>
+        <select
+          id="theater"
+          value={selectedTheater}
+          onChange={(e) => setSelectedTheater(e.target.value)}
+        >
+          <option value="">Select a theater</option>
+          <option value="1">Campus Theater</option>
+          <option value="2">South Theater</option>
+          <option value="3">Northwest Theater</option>
+          <option value="4">Southwest Theater</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="time">Time:</label>
+        <input
+          type="time"
+          id="time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="seat">Seat:</label>
+        <input
+          type="number"
+          id="seat"
+          min="1"
+          max="30"
+          value={seat}
+          onChange={(e) => setSeat(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="owner">Name:</label>
+        <input
+          type="text"
+          id="owner"
+          value={owner}
+          onChange={(e) => setOwner(e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={() => navigate('/UserHome')}>Cancel</button>
+        <button onClick={handleConfirmPurchase}>Confirm Purchase</button>
+      </div>
     </div>
   );
 };
 
 export default Payment;
+
