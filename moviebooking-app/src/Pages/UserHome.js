6663870c5012778fe38d9f7766e691f01d1fca36
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-05-02 23:16:16 -0500

    Update UserHome.js

--- moviebooking-app/src/Pages/UserHome.js
+++ moviebooking-app/src/Pages/UserHome.js
@@ -11,7 +11,7 @@ const UserHome = () =>{
                 <h1>User Home Page</h1>
                 <Link to = '/browse'><button id = "user-home" ><img src = {BrowseImg} id = "user-home"/></button></Link>
                 <Link to = '/movieUpcoming' ><button id = "user-home"><img src = {BrowseImg2} id = "user-home"/></button></Link>
-                <Link to = '/usertickets'><button id = "user-home"><img src = {BrowseImg3} id = "user-home"/></button></Link>
+                
                 
             </p>
 
