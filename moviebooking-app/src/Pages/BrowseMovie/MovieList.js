ce5a8e3b5663d881327c203930566c8bed780fa9
valequintanilla <108659282+valequintanilla@users.noreply.github.com>
2023-05-02 19:03:31 -0500

    Update MovieList.js

--- moviebooking-app/src/Pages/BrowseMovie/MovieList.js
+++ moviebooking-app/src/Pages/BrowseMovie/MovieList.js
@@ -8,7 +8,7 @@ const MovieList = (props) => {
 			{props.movies.map((movie, index) => (
 				<div className='image-container d-flex justify-content-start m-3'>
 					<div className='top-left'>
-						<h2 style={{color :'darkorange'}}>{movie.Title}</h2>
+						<h5 style={{color :'darkorange'}}>{movie.Title}</h5>
 						<img src={movie.Poster} alt='movie'></img>
 						<div
 							onClick={() => props.handleClick(movie)}
@@ -18,16 +18,21 @@ const MovieList = (props) => {
 							
 						</div>
 					</div>
-					<div>
+					<box style={{ 
+						width: '700px',
+						inlinesize: '350px',
+						height: '100px',
+						blocksize: '150px',
+						margin: '10px',
+						padding: '25px',
+						border: '5px solid black'}}>
+						<br></br>
 						<h6>Movie Summary:</h6>
 						<p>{movie.Summary}</p>
 						<h6>Release Year: {movie.Year}</h6>
 						<h6>Duration: {movie.Duration}</h6>
-						<h6 style={{
-            color :'darkorange'
-            
-          }}>{movie.Rate}</h6>
-					</div>
+						<h6 style={{color :'darkorange'}}>{movie.Rate}</h6>
+					</box>
 					
 				</div>
 			))}
