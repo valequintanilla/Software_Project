d0d6536e31190ddad29a410cdbf0d2e04300126b
julesgar <87442019+julesgar@users.noreply.github.com>
2023-05-02 23:21:19 -0500

    Update ManageShow.js

--- moviebooking-app/src/Pages/ManageShow.js
+++ moviebooking-app/src/Pages/ManageShow.js
@@ -1,108 +1,173 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import MovieCatalog from './MovieCatalog';
-import { getMovies, deleteMovie } from '../API_Calls/API';
-import { Movie } from '../classes';
-import UpcomingMovieList from './BrowseMovie/UpcomingMovieList';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Ticket } from '../classes';
+import { bookTicket } from '../API_Calls/API';
+import Barcode from 'react-barcode';
 
-const ManageShows = () => {
-  const [selectedMovie, setSelectedMovie] = useState(null);
-  const [deleteMode, setDeleteMode] = useState(false);
-    
-  const [movies, setMovies] = useState([{
-    Title: "The Super Mario Bros. Movie",
-    Year: "2023",
-    imdbID: "tt6718170",
-    Type: "movie",
-    Poster: "https://m.media-amazon.com/images/M/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_SX300.jpg",
-    Summary: "While working underground to fix a water main, Brooklyn plumbers Mario (Chris Pratt) and brother Luigi (Charlie Day) are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi. With the assistance of Mushroom Kingdom resident Toad (Keegan-Michael Key) and some training from the strong-willed ruler of the Mushroom Kingdom, Princess Peach (Anya Taylor-Joy), Mario taps into his own power.", 
-    Duration: "1hr 32min",
-    Rate: "PG",
-    Tickets: 3
-    
-  },{
-    Title: "Creed III",
-    Year: "2023",
-    imdbID: "tt11145118",
-    Type: "movie",
-    Poster: "https://m.media-amazon.com/images/M/MV5BYWY1ZDY4MmQtYjhiYS00N2QwLTk1NzgtOWI2YzUwZThjNDYwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg",
-    Summary: "After dominating the boxing world, Adonis Creed (Michael B. Jordan) has been thriving in both his career and family life. When a childhood friend and former boxing prodigy, Damian (Jonathan Majors), resurfaces after serving a long sentence in prison, he is eager to prove that he deserves his shot in the ring. The face off between former friends is more than just a fight. To settle the score, Adonis must put his future on the line to battle Damian – a fighter who has nothing to lose. Creed III is the third installment in the successful franchise and is Michael B. Jordan’s directorial debut.", 
-    Duration: "1hr 56min",
-    Rate: "PG-13",
-    Tickets: 3
-  },{
-    Title: "Hush! Girls Don't Scream",
-    Year: "2013",
-    imdbID: "tt2440036",
-    Type: "movie",
-    Poster: "https://m.media-amazon.com/images/M/MV5BNzA1Nzk2Mzc0Nl5BMl5BanBnXkFtZTgwNDQzNzc2MDE@._V1_SX300.jpg",
-    Summary: "Following the latest Ghostface killings, the four survivors leave Woodsboro behind and start a fresh chapter. In Scream VI, Melissa Barrera (“Sam Carpenter”), Jasmin Savoy Brown (“Mindy Meeks-Martin”), Mason Gooding (“Chad Meeks-Martin”), JennaOrtega (“Tara Carpenter”), Hayden Panettiere (“Kirby Reed”) and Courteney Cox (“Gale Weathers”) return to their roles in the franchise alongside Jack Champion, Henry Czerny, Liana Liberato, Dermot Mulroney, Devyn Nekoda, Tony Revolori, Josh Segarra, and Samara Weaving.", 
-    Duration: "2hr 2min",
-    Rate: "R",
-    Tickets: 3
-  },{
-    Title: "Shazam! Fury of the Gods",
-    Year: "2023",
-    imdbID: "tt10151854",
-    Type: "movie",
-    Poster: "https://m.media-amazon.com/images/M/MV5BNzJlM2NmZTItOGQyYS00MmE2LTkwZGUtNDFkNmJmZjRjZjcxXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_SX300.jpg",
-    Summary: "From New Line Cinema comes “Shazam! Fury of the Gods,” which continues the story of teenage Billy Batson who, upon reciting the magic word “SHAZAM!,” is transformed into his adult Super Hero alter ego, Shazam.", 
-    Duration: "2hr 10min",
-    Rate: "PG-13",
-    Tickets: 3
-  }]);
+const Payment = ({ shoppingCart, incrementMovieTickets }) => {
+  const navigate = useNavigate();
+  const [movieDetails, setMovieDetails] = useState(
+    shoppingCart.map((movie) => ({
+      movieId: movie.imdbID,
+      theater: '',
+      time: '',
+      seat: '',
+      owner: '',
+    }))
+  );
 
-  const handleDelete = async () => {
-    if (!selectedMovie) return;
-    const status = await deleteMovie(selectedMovie);
-    console.log('Movie deleted with status:', status);
-    setMovies(movies.filter(movie => movie.id !== selectedMovie));
-    setSelectedMovie(null);
-    setDeleteMode(false);
+  const [paymentMethod, setPaymentMethod] = useState('');
+  const [purchasedTickets, setPurchasedTickets] = useState([]);
+
+  const updateMovieDetails = (index, key, value) => {
+    const updatedDetails = [...movieDetails];
+    updatedDetails[index][key] = value;
+    setMovieDetails(updatedDetails);
+  };
+
+  const handleConfirmPurchase = async () => {
+    if (
+      !movieDetails.every(
+        (movie) =>
+          movie.theater && movie.time && movie.seat && movie.owner
+      )
+    ) {
+      alert('Please fill in all required fields for each movie.');
+
+      return;
+    }
+
+    const purchased = [];
+
+    for (const movie of movieDetails) {
+      const ticket = new Ticket(
+        movie.movieId,
+        movie.theater,
+        movie.time,
+        movie.seat,
+        movie.owner
+      );
+      purchased.push(ticket);
+
+      try {
+        const status = await bookTicket(ticket);
+        console.log(`Ticket booking status: ${status}`);
+        incrementMovieTickets(movie.movieId);
+      } catch (err) {
+        console.error('Error booking ticket:', err);
+      }
+    }
+    setPurchasedTickets(purchased);
+  };
+
+  const renderTicket = (ticket) => {
+    const movie = shoppingCart.find((movie) => movie.imdbID === ticket.movieId);
+    return (
+      <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+        <h3>{movie.Title}</h3>
+        <p>Theater Location: {ticket.theater}</p>
+        <p>Time: {ticket.time}</p>
+        <p>Seat: {ticket.seat}</p>
+        <p>Name: {ticket.owner}</p>
+        <Barcode value={ticket.id} />
+      </div>
+    );
   };
 
   return (
     <div>
-      <h1>Manage Show</h1>
-      <div className='row'>
-				<UpcomingMovieList
-					movies={movies}
-				/>
-			</div>
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
-        <button
-          style={{ backgroundColor: 'white', color: 'black', padding: '10px' }}
-          onClick={() => setDeleteMode(!deleteMode)}
-        >
-          Remove
-        </button>
-        {deleteMode && (
+      <h1>Payment</h1>
+      {shoppingCart.map((movie, index) => (
+        <div key={movie.imdbID}>
+          <h3>{movie.Title}</h3>
           <div>
-            <button
-              style={{ backgroundColor: 'white', color: 'black', padding: '10px' }}
-              onClick={() => setDeleteMode(false)}
-            >
-              Cancel
-            </button>
-            <button
-              style={{ backgroundColor: 'white', color: 'black', padding: '10px' }}
-              //onClick={handleDelete}
-            >
-              Delete
-            </button>
+            <label htmlFor="paymentMethod">Payment Method:</label>
+            
+              <option value="creditCard">Credit Card</option>
+              <option value="debitCard">Debit Card</option>
+              <option value="paypal">PayPal</option>
+            
           </div>
-        )}
-        <div>
-          <Link to="/add-show">
-            <button style={{ backgroundColor: 'white', color: 'black', padding: '10px', marginRight: '10px' }}>Add</button>
-          </Link>
-          <Link to="/adminhome">
-            <button style={{ backgroundColor: 'white', color: 'black', padding: '10px' }}>Homepage</button>
-          </Link>
         </div>
+      ))}
+      <div>
+        <button onClick={() => setPaymentMethod('credit')}>Credit</button>
+        <button onClick={() => setPaymentMethod('debit')}>Debit</button>
+        <button onClick={() => setPaymentMethod('paypal')}>PayPal</button>
       </div>
+      {paymentMethod === 'credit' && (
+        <div>
+          <h2>Credit Card Information</h2>
+          <input type="text" placeholder="Card Number" />
+          <input type="text" placeholder="Expiration Date" />
+          <input type="text" placeholder="CVV" />
+        </div>
+      )}
+      {paymentMethod === 'debit' && (
+        <div>
+          <h2>Debit Card Information</h2>
+          <input type="text" placeholder="Card Number" />
+          <input type="text" placeholder="Expiration Date" />
+          <input type="text" placeholder="CVV" />
+        </div>
+      )}
+            {paymentMethod === 'paypal' && (
+        <div>
+          <h2>PayPal Information</h2>
+          <input type="text" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+      )}
+      {shoppingCart.map((movie, index) => (
+        <div key={movie.imdbID}>
+          <label htmlFor={`theater-${index}`}>Theater Location:</label>
+          <select
+            id={`theater-${index}`}
+            value={movieDetails[index].theater}
+            onChange={(e) =>
+              updateMovieDetails(index, 'theater', e.target.value)
+            }
+          >
+            <option value="">Select a theater</option>
+            <option value="1">Campus Theater</option>
+            <option value="2">South Theater</option>
+            <option value="3">Northwest Theater</option>
+            <option value="4">Southwest Theater</option>
+          </select>
+          <label htmlFor={`time-${index}`}>Time:</label>
+          <input
+            type="time"
+            id={`time-${index}`}
+            value={movieDetails[index].time}
+            onChange={(e) => updateMovieDetails(index, 'time', e.target.value)}
+          />
+          <label htmlFor={`seat-${index}`}>Seat:</label>
+          <input
+            type="number"
+            id={`seat-${index}`}
+            min="1"
+            max="30"
+            value={movieDetails[index].seat}
+            onChange={(e) => updateMovieDetails(index, 'seat', e.target.value)}
+          />
+          <label htmlFor={`owner-${index}`}>Name:</label>
+          <input
+            type="text"
+            id={`owner-${index}`}
+            value={movieDetails[index].owner}
+            onChange={(e) => updateMovieDetails(index, 'owner', e.target.value)}
+          />
+        </div>
+      ))}
+      <div>
+        <button onClick={() => navigate('/Userhome')}>Cancel</button>
+        <button onClick={handleConfirmPurchase}>Confirm Purchase</button>
+      </div>
+      {purchasedTickets.map((ticket) => renderTicket(ticket))}
+
     </div>
   );
 };
 
-export default ManageShows;
+export default Payment;
+
